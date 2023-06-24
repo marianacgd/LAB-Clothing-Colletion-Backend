052a82bf0beec04a2a391f1e46af1d2df1c11058
Mariana Daruix <mariana.daruix@gmail.com>
2023-06-24 04:14:30 -0300

    Exercicio S02 Concluido

--- LABClothingCollection/LABClothingCollection.API/DTO/Usuarios/UsuarioUpdateDTO.cs
+++ LABClothingCollection/LABClothingCollection.API/DTO/Usuarios/UsuarioUpdateDTO.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace LABClothingCollection.API.DTO.Usuarios
 {
-    public class UsuarioCreateDTO
+    public class UsuarioUpdateDTO
     {
         [Required(ErrorMessage = "Campo Obrigatório")]
         [MaxLength(250, ErrorMessage = "Este campo aceita até 250 caracteres")]
@@ -19,28 +19,14 @@ namespace LABClothingCollection.API.DTO.Usuarios
         [Display(Name = "Data de Nascimento")]
         public DateTime DataNascimento { get; set; }
 
-        [Required(ErrorMessage = "Campo Obrigatório")]
-        [MaxLength(18, ErrorMessage = "Este campo aceita até 18 caracteres")]
-        [MinLength(11, ErrorMessage = "Favor digitar o minimo de 11 caracteres")]
-        [Display(Name = "CPF/CNPJ")]
-        public string Documento { get; set; }
-
         [MaxLength(11, ErrorMessage = "Este campo aceita até 11 caracteres")]
         [MinLength(11, ErrorMessage = "Favor digitar um número de telefone")]
         [Display(Name = "Telefone")]
         public string? Telefone { get; set; }
 
         [Required(ErrorMessage = "Campo Obrigatório")]
-        [Display(Name = "E-mail")]
-        [EmailAddress(ErrorMessage = "Email Erradp")]
-        public string Email { get; set; }
-
-        [Required(ErrorMessage = "Campo Obrigatório")]
         [Display(Name = "Tipo de Usuário")]
         public TipoUsuarioEnum Tipo { get; set; }
-
-        [Required(ErrorMessage = "Campo Obrigatório")]
-        [Display(Name = "Status de Usuário")]
-        public StatusEnum Status { get; set; }
     }
+
 }
