c05ae3cabd60144f8e2f5580ec47d7e3937f0ffd
Mariana Daruix <mariana.daruix@gmail.com>
2023-06-24 23:14:15 -0300

    Exercicio S8 Finalizado

---
+++ LABClothingCollection/LABClothingCollection.API/DTO/Colecoes/ColecaoUpdateStatusDTO.cs
@@ -0,0 +1,20 @@
+﻿using LABClothingCollection.API.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace LABClothingCollection.API.DTO.Colecoes
+{
+    public class ColecaoUpdateStatusDTO : IValidatableObject
+    {
+        [Display(Name = "Status da Coleção")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public StatusEnum StatusSistema { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!Enum.IsDefined(typeof(StatusEnum), StatusSistema))
+            {
+                yield return new ValidationResult($"Erro no status da coleção", new[] { nameof(StatusSistema) });
+            }
+        }
+    }
+}
